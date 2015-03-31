e80ee16d0ff75f07e464f4a4b352d774efc4637c
Elastiskalinjen <sebastian.tvilling@hotmail.com>
2015-03-31 14:31:43 +0200

    Fixat lite och gjort klasser, dispenser, engineerTower, missile

---
+++ Steam_Hunters/Steam Hunters/Steam_Hunters/GameObject/Dispenser.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Steam_Hunters
+{
+    class Dispenser:GameObject
+    {
+        public Color color = Color.White;
+        public int spriteWidth, spriteHeight;
+        private float radius, dispenserPower;
+        bool dispenserRemove;
+
+        public float Radius
+        {
+            get { return radius; }
+
+            set
+            {
+                radius = value;
+            }
+        }
+
+        public bool DispenserRemove
+        {
+            get { return dispenserRemove; }
+        }
+
+        public Dispenser(Texture2D tex, Vector2 pos, float radius)
+            : base(tex,pos)
+        {
+            spriteWidth = 50;//måste titta vad den har för värden! 
+            spriteHeight = 50;//måste titta vad den har för värden! 
+            this.radius = radius;
+ 
+            center = new Vector2(pos.X + spriteWidth / 2, pos.Y + spriteHeight / 2);
+            origin = new Vector2(spriteWidth / 2, spriteHeight / 2);
+        }
+
+         public override void Update(GameTime gameTime)
+         {
+             this.center = new Vector2(pos.X + tex.Width / 2, pos.Y + tex.Height / 2);
+             dispenserPower += (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+             if (dispenserPower == 20)
+                 dispenserRemove = true;
+
+             hitBox = new Rectangle((int)pos.X, (int)pos.Y, spriteWidth, spriteHeight);
+         }
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(tex, center, null, color, rotation, origin, 1.0f, SpriteEffects.None, 0);     
+        }
+
+        public bool IsInRange(Vector2 pos)
+        {
+            return Vector2.Distance(center, pos) <= radius;
+        }
+    }
+}
+
+
---
+++ Steam_Hunters/Steam Hunters/Steam_Hunters/GameObject/EngineerTower.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Steam_Hunters
+{
+    class EngineerTower:GameObject
+    {
+        public Color color = Color.White;
+        int spriteWidth, spriteHeight;
+        private float radius, bulletTimer;
+        GamePlayScreen gps;
+        bool towerRemove;
+        float towerPower;
+        public int towerLife;
+
+        public bool TowerRemove
+        {
+            get { return towerRemove; }
+        }
+
+         public EngineerTower(Texture2D tex, Vector2 pos, GamePlayScreen gps, int towerLife)
+            : base(tex,pos)
+        {
+            this.gps = gps;
+            spriteWidth = 50;//är något annnat ! 
+            spriteHeight = 50;//är något annnat ! 
+            this.towerLife = towerLife;
+ 
+            center = new Vector2(pos.X + spriteWidth / 2, pos.Y + spriteHeight / 2);
+            origin = new Vector2(spriteWidth / 2, spriteHeight / 2);
+        }
+
+         public override void Update(GameTime gameTime)
+         {
+             towerPower+= (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+             if (towerPower == 90)
+             {
+                 towerRemove = true;
+             }
+
+             //if (bulletTimer >= 0.75f && target != null)
+             //{
+             //    TowerBullet tbullet = new TowerBullet(TextureManager.towerBulletTexture, Vector2.Subtract(center,
+             //        new Vector2(TextureManager.towerBulletTexture.Width / 2)), rotation, 9);
+
+             //    gps.tbullets.Add(tbullet);
+             //    bulletTimer = 0;
+             //}
+         }
+         public override void Draw(SpriteBatch spriteBatch)
+         {
+             spriteBatch.Draw(tex, center, null, color, rotation, origin, 1.0f, SpriteEffects.None, 0);
+         }
+    }
+}
