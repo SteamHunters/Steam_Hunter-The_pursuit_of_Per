e106ee599c146953ad79a3a2770c18a701b6601a
macke33 <markus-kareby@hotmail.com>
2015-04-12 18:05:43 +0200

    Enemy random movement nästan klar
    ka inte röra sig ner men annars går de

--- Steam_Hunters/Steam Hunters/Steam_Hunters/Entity/Enemies.cs
+++ Steam_Hunters/Steam Hunters/Steam_Hunters/Entity/Enemies.cs
@@ -13,13 +13,27 @@ namespace Steam_Hunters
          float AttackSpeed, float MapPos, bool Aggro, float AttackCooldown)
             : base(tex, pos, frameSize, sheetSize, Hp, MaxHp, Gold, Item, AttackRangeRadius, SearchRadius, MovementSpeed, AttackSpeed, MapPos, Aggro, AttackCooldown)
         {
+
         }
+        double totalElapsedSeconds = 0;
+        const double MovementChangeTimeSeconds = 2.0; //seconds
+
+
         public override void Update(GameTime gameTime)
         {
             base.Update(gameTime);
 
             if (Aggro == false)
             {
+                totalElapsedSeconds += gameTime.ElapsedGameTime.TotalSeconds;
+
+                if (totalElapsedSeconds >= MovementChangeTimeSeconds)
+                {
+                    totalElapsedSeconds -= MovementChangeTimeSeconds;
+                    this.direction = GetRandomDirection();
+                }
+
+                pos += direction;
 
             }
         }
@@ -28,28 +42,27 @@ namespace Steam_Hunters
 
             base.Draw(spriteBatch);
         }
-        //Vector2 GetRandomDirection()
-        //{
-        //    Random random = new Random();
-        //    int randomDirection = random.Next(4);
+        Vector2 GetRandomDirection()
+        {
+            Random random = new Random();
+            int randomDirection = random.Next(4);
 
-        //    switch (randomDirection)
-        //    {
-        //        case 1:
-        //            return new Vector2(-1, 0);
-        //        case 2:
-        //            return new Vector2(1, 0);
-        //        case 3:
-        //            return new Vector2(0, -1);
-        //        case 4:
-        //            return new Vector2(0, 1);
-        //        default:
-        //            return Vector2.Zero;
-        //    }
-        //}
+            switch (randomDirection)
+            {
+                case 1:
+                    return new Vector2(-1, 0);
+                case 2:
+                    return new Vector2(1, 0);
+                case 3:
+                    return new Vector2(0, -1);
+                case 4:
+                    return new Vector2(0, 1);
+                default:
+                    return Vector2.Zero;
+            }
+        }
     }
 }
-
 
 
 
